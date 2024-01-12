9be44af8861931bd470d8912ed661956c6f01f4d
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-12 09:10:07 +0900

    init

---
+++ src/test/java/com/example/helloCar/HelloCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
