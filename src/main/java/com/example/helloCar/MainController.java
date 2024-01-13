aae14882b6d55fea8ee9fe0d6bc520c954ecbf60
choiKYeon <gusrudchl12@gmail.com>
2024-01-13 14:49:04 +0900

    test

--- src/main/java/com/example/helloCar/MainController.java
+++ src/main/java/com/example/helloCar/MainController.java
@@ -2,14 +2,12 @@ package com.example.helloCar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/home")
-    @ResponseBody
-    public String test(){
-        return "안녕하세요";
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/hellocar/start";
     }
 }
