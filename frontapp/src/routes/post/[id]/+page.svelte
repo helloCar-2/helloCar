a354e41e37c8d4760a1f0a643328ee807b0182b1
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-23 15:26:17 +0900

    svelte front test

---
+++ frontapp/src/routes/post/[id]/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+</script>
+
+<div>
+	<div>게시글 상세</div>
+
+	<h3>{data.id}</h3>
+	<h1>{data.title}</h1>
+
+	<img src={data.thumbnail} />
+    <p>{data.description}</p>
+</div>
