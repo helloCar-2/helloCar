3989ef740365d86ab2d3d786d5b129f60cd86039
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-23 15:26:17 +0900

    svelte front test

---
+++ frontapp/src/routes/post/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let data;
+</script>
+
+<ul>
+    {#each data.products as product}
+    <li>
+        <img src="{product.thumbnail}" alt="">
+
+        <a href="/post/{product.id}">
+        {product.title}
+        </a>
+
+
+    </li>
+    {/each}
+</ul>
