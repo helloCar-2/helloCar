b3de9fe53faa7e1b1baf150489a89b54fb48cc70
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-24 12:47:09 +0900

    학원에서 단건 다건 조회 해봄

--- frontapp/src/routes/post/+page.js
+++
@@ -1,8 +0,0 @@
-export async function load ({fetch}){
-    let res = await fetch("https://dummyjson.com/products");
-    let result = await res.json();
-    console.log(result);
-
-    return result;
-
-}
