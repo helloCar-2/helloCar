35d35108f94332cd5fa1bf1edf55ad557fc1bdd5
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-02-07 10:08:21 +0900

    카카오 앱키 숨김

--- frontapp/src/routes/oauth/callback/kakao/+page.svelte
+++ frontapp/src/routes/oauth/callback/kakao/+page.svelte
@@ -2,10 +2,11 @@
 	import { onMount } from 'svelte';
 
 	onMount(async () => {
-		// URL에서 인증 코드(code) 추출
+		// URL에서 인증 코드(code) 추출.
 		const urlParams = new URLSearchParams(window.location.search);
+
+		// code 라는 변수에 카카오에서 발급한 코드를 넣음.
 		const code = urlParams.get('code');
-		console.log(code);
 
 		if (code) {
 			// 인증 코드가 유효하면, 이를 서버에 전송
@@ -16,35 +17,38 @@
 				},
 				body: new URLSearchParams({
 					grant_type: 'authorization_code',
-					client_id: '0db93a1c1e0a9813a3d10461ca9a40d2',
+					client_id: import.meta.env.VITE_CLIENT_ID,
 					redirect_uri: 'http://localhost:5173/oauth/callback/kakao',
 					code: code
 				})
 			});
 
+			// data 라는 변수에 서버에서 받아온 정보 response를 json 형태로 저장시킴.
 			const data = await response.json();
+
+			// data에서 액세스 토큰 추출.
 			const access_token = data.access_token;
 
-			// 액세스 토큰을 받았으므로, 이를 이용하여 필요한 작업 수행
-			console.log('액세스 토큰:', access_token);
-			localStorage.setItem('accessToken', access_token);
-
+			// 추출한 액세스 토큰으로 다시 카카오 서버에 유저 정보 요청
 			const userInfoResponse = await fetch('https://kapi.kakao.com/v2/user/me', {
 				method: 'GET',
 				headers: {
 					Authorization: `Bearer ${access_token}`
 				}
 			});
-			if (userInfoResponse.ok) {
-				const userInfo = await userInfoResponse.json();
-				console.log(userInfo.id);
-				console.log(userInfo.properties.nickname);
 
+			// 서버에서 받아온 userInfoResponse 변수에 담긴 ok값이 true라면
+			if (userInfoResponse.ok) {
+				// userInfo 라는 변수에 userInfoResponse를 json 타입으로 저장시킴.
+				const userInfo = await userInfoResponse.json();
+
+				// formData 라는 변수에 카카오 서버에서 받아온 유저 고유의 id값, 닉네임값을 저장시킴.
 				let formData = {
 					id: `${userInfo.id}`,
 					nickname: `${userInfo.properties.nickname}`
 				};
 
+				// formData를 백엔드 서버로 보내, 로그인 요청 시도
 				const res = await fetch(`http://localhost:8080/api/v1/member/kakaologin`, {
 					method: 'POST',
 					headers: {
@@ -53,15 +57,18 @@
 					body: JSON.stringify(formData)
 				});
 
+				// 백엔드 서버에서 받은 res 변수에 담긴 ok값이 true 라면
 				if (res.ok) {
 					const data = await res.json();
+
+					// 액세스 토큰과 리프레시 토큰을 각각 변수에 저장시키고
 					const accesstoken = await data.data.accessToken;
 					const refreshToken = await data.data.refreshToken;
+
+					// 두 토큰을 로컬 스토리지에 저장 후 홈 페이지로 이동
 					await localStorage.setItem('accessToken', accesstoken);
 					await localStorage.setItem('refreshToken', refreshToken);
-					const storedToken = await localStorage.getItem('accessToken');
-					await console.log(storedToken);
-                    window.location.href = '/car-home';
+					window.location.href = '/car-home';
 				}
 			}
 		}
